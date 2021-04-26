46edc8a22091094512478d83f1df0a73c9e3a10f
BrkCoder <baribarakinbal@gmail.com>
2021-04-26 10:21:01 +0300

    add first test which passed & failed

---
+++ cypress/integration/tests/test.spec.ts
@@ -0,0 +1,11 @@
+
+
+context('My First Test', () => {
+    it('should pass', () => {
+        expect(true).to.equal(true)
+    })
+
+    it('should failed', () => {
+        expect(true).to.equal(false)
+    })
+});
